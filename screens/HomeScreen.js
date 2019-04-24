bda3ae36e08c1206df37043d91e6fc610d215f7c
Laetitia <laetitia.corre6@gmail.com>
2019-04-24 16:42:33 +0200

    header placement

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-class HomeScreen extends React.Component {
+import React from "react";
+import { View, Text } from "react-native";
 
-    static navigationOptions = ({ navigation }) => {
-        return {
-            header: null,
-        };
-    }
-    render() {
-        return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>HomeScreen</Text>
-            </View>
-        );
-    }
+import Headernav from "../components/layouts/header";
+
+class HomeScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      header: null
+    };
+  };
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Headernav drawerNav={this.props.navigation} />
+        <Text>HomeScreen</Text>
+      </View>
+    );
+  }
 }
 
 export default HomeScreen;
