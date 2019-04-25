b7f48d1820146d51d8122cbad7dd62d81b111024
Patouillard_Franck <patouillardfranck3@gmail.com>
2019-04-25 08:58:51 +0000

    Formik installation
    Insertion formik

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, View } from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={ styles.container }>
       </View>
     );
   }
@@ -17,5 +15,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
