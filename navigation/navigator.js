1b60b9723341f702149ff50ef1a31bc2b98e456d
Kevin Doquero <k.doquero@aidodys.com>
2019-04-25 10:42:23 +0200

    added commentary inside readme and inside navigator.js

--- navigation/navigator.js
+++ navigation/navigator.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { createStackNavigator, createBottomTabNavigator,createAppContainer,createDrawerNavigator } from 'react-navigation';
+
 import HomeStack from './stack/HomeStack';
+/**
+ * Creation d'un Drawer navigator a partir des stacks  https://reactnavigation.org/docs/en/drawer-navigator.html
+ * Vous pouvez ajouter votre stack ici pour afficher votre page dans le menu drawer
+ * ex : Home: HomeStack, Faq:FaqStack
+ */
 const DrawerNavigator = createDrawerNavigator({
         Home: HomeStack,
     }
 ); 
    
-  
+/**
+ * Le root navigator regroupe toute les stack et les navigators
+ */ 
 const RootStack = createStackNavigator({
         Main: DrawerNavigator,
     }, {
--- navigation/stack/HomeStack.js
+++ navigation/stack/HomeStack.js
@@ -4,9 +4,15 @@ import { createStackNavigator, createBottomTabNavigator } from 'react-navigation
 
 import HomeScreen from '../../screens/HomeScreen';
 
+/**
+ * Creation d'un Stack Navigator à partir d'un Screen https://reactnavigation.org/docs/en/stack-navigator.html
+ */
 const HomeStack = createStackNavigator({        
   	Home: HomeScreen 
 });
+/**
+ * Nom du label affiché dans le menu
+ */
 HomeStack.navigationOptions = {
     tabBarLabel: 'Home'
 };
