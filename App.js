68a051405c44c6ac66bffbf0eb6f727b69c3611f
ccsr <clement.causer@gmail.com>
2019-04-25 13:58:22 +0200

    Analytics initialized and working. Updated README.md with markdown Config.js set up

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
 import Navigator from "./navigation/navigator";
 import { Provider } from "react-redux";
 import store from "./store";
-export default class App extends React.Component {
+import { Amplitude } from "expo";
+import { analyticsAPIKey } from "./config";
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    Amplitude.initialize(analyticsAPIKey);
+  }
   render() {
     return (
       <Provider store={store}>
@@ -16,8 +22,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
