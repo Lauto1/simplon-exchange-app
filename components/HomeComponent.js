082b8bef2bc6f32f00f0e45b9506e2aabc5fdf1d
Kevin Doquero <k.doquero@aidodys.com>
2019-04-25 14:28:22 +0200

    question reducer ,action question

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -19,201 +19,6 @@ import Searchbar from "../components/Searchbar";
 import Headernav from "../components/layouts/header";
 import ScrollToTopButton from "./ScrollToTopButton";
 
-const QUESTIONS = [
-  {
-    id: "01",
-    title: "Question 1 à propos de ",
-    date: "09/10/2018",
-    author: "Jean-Michel",
-    content: "Ici gît le corps de ma question 1",
-    upvote: 0,
-    answers: [
-      {
-        id: "6",
-        author: "Marie-Béatrice",
-        karma: 0,
-        content: "La super réponse de Marie-Béatrice"
-      },
-      {
-        id: "7",
-        author: "Marie-Béatrice",
-        karma: 0,
-        content: "La nouvelle réponse de Marie-Béatrice"
-      },
-      {
-        id: "9",
-        author: "Jean-Michel",
-        karma: 0,
-        content: "La réponse de Jean-Michel à Marie-Béatrice"
-      },
-      {
-        id: "19",
-        author: "Marie-Béatrice",
-        karma: 0,
-        content: "La réponse de Marie-Béatrice à Jean-Michel"
-      },
-      {
-        id: "109",
-        author: "Al Capone",
-        karma: 90,
-        content: "Le dernier mot est donné par Al Capone"
-      }
-    ],
-    factory: "Villeurbanne",
-    tag: "php"
-  },
-  {
-    id: "02",
-    title: "Question 2 à propos de ",
-    date: "09/11/2018",
-    author: "Thor",
-    content: "Ici est le corps de ma question 2",
-    upvote: 3,
-    answers: [
-      {
-        id: "555",
-        author: "JCVD",
-        karma: 7,
-        content: "La super réponse de JCVD"
-      }
-    ],
-    factory: "Vénissieux",
-    tag: "mysql"
-  },
-  {
-    id: "03",
-    title: "Question 3 à propos de ",
-    date: "05/11/2016",
-    author: "Garcimore",
-    content: "Ici est le corps de ma question 3",
-    upvote: 2,
-    answers: [
-      {
-        id: "666",
-        author: "Le pape",
-        karma: 7,
-        content: "La super réponse du Pape"
-      }
-    ],
-    factory: "Roanne",
-    tag: "css"
-  },
-  {
-    id: "04",
-    title: "Question 4 à propos de ",
-    date: "05/04/2017",
-    author: "Casimir",
-    content: "Ici est le texte de la question 4",
-    upvote: 0,
-    answers: [
-      {
-        id: "11",
-        author: "Messi",
-        karma: 77,
-        content: "La super réponse de Messi"
-      }
-    ],
-    factory: "Montpellier",
-    tag: "Ruby"
-  },
-  {
-    id: "05",
-    title: "Question 5 à propos de ",
-    date: "05/04/2017",
-    author: "Casimir",
-    content: "Ici est le texte de la question 5",
-    upvote: 0,
-    answers: [
-      {
-        id: "55",
-        author: "Messi",
-        karma: 77,
-        content: "La super réponse de Messi"
-      },
-      {
-        id: "999",
-        author: "Le Messie",
-        karma: 888,
-        content: "La super réponse du Messie"
-      }
-    ],
-    factory: "Montpellier",
-    tag: "Ruby"
-  },
-  {
-    id: "06",
-    title: "Question 6 à propos de ",
-    date: "05/04/2017",
-    author: "Casimir",
-    content: "Ici est le texte de la question 6",
-    upvote: 0,
-    answers: [
-      {
-        id: "55",
-        author: "Messi",
-        karma: 77,
-        content: "La super réponse de Messi"
-      },
-      {
-        id: "999",
-        author: "Le Messie",
-        karma: 888,
-        content: "La super réponse du Messie"
-      }
-    ],
-    factory: "Montpellier",
-    tag: "Ruby"
-  },
-  {
-    id: "07",
-    title: "Question 7 à propos de ",
-    date: "05/04/2017",
-    author: "Casimir",
-    content: "Ici est le texte de la question 7",
-    upvote: 0,
-    answers: [
-      {
-        id: "55",
-        author: "Messi",
-        karma: 77,
-        content: "La super réponse de Messi"
-      },
-      {
-        id: "999",
-        author: "Le Messie",
-        karma: 888,
-        content: "La super réponse du Messie"
-      }
-    ],
-    factory: "Montpellier",
-    tag: "Ruby"
-  },
-  {
-    id: "08",
-    title: "Question 8 à propos de ",
-    date: "05/04/2017",
-    author: "Casimir",
-    content: "Ici est le texte de la question 8",
-    upvote: 0,
-    answers: [
-      {
-        id: "55",
-        author: "Messi",
-        karma: 77,
-        content: "La super réponse de Messi"
-      },
-      {
-        id: "999",
-        author: "Le Messie",
-        karma: 888,
-        content: "La super réponse du Messie"
-      }
-    ],
-    factory: "Montpellier",
-    tag: "Ruby"
-  }
-];
-
 class HomeComponent extends React.Component {
   state = {
     questions: [],
@@ -221,15 +26,20 @@ class HomeComponent extends React.Component {
   };
 
   componentDidMount() {
+      this.props.actions.fetchQuestions().then(questions =>{
+        console.log(questions, "promise");
+      }
+      )
+      
     // Il faudra récupérer les questions via l'API quand elle sera prête, par exemple:
     // fetch('/api/questions')
     //   .then(response => response.json())
     //   .then(questions => this.setState({ questions }));
     //
     // Pour l'instant, on fait semblant que les questions prennent 1 second à arriver
-    setTimeout(() => {
-      this.setState({ questions: QUESTIONS });
-    }, 1000);
+    // setTimeout(() => {
+    //   this.setState({ questions: QUESTIONS });
+    // }, 1000);
   }
 
   onScrollTop = () => {
@@ -250,12 +60,12 @@ class HomeComponent extends React.Component {
   };
 
   render() {
-    const { questions, showScrollToTop } = this.state;
-
+    const {  showScrollToTop } = this.state;
+    const  questions  = this.props.questions;
     return (
       <View style={styles.view}>
         <Headernav drawerNav={this.props.drawerNav} />
-        <Searchbar style={styles.search} />
+        <Searchbar searchQuestion={this.props.actions.searchQuestions} style={styles.search} />
         <ScrollView
           style={styles.contentContainer}
           ref="scrollView"
