b59c50ca7e137f503937c80e990edbd3aedbbc99
Laurie.M <laurie.martineau.ext@homeserve.fr>
2019-04-26 14:45:09 +0200

    changes

--- navigation/stack/HomeStack.js
+++ navigation/stack/HomeStack.js
@@ -5,6 +5,7 @@ import { createStackNavigator, createBottomTabNavigator } from 'react-navigation
 import HomeScreen from '../../screens/HomeScreen';
 import FaqScreen from '../../screens/FaqScreen';
 import BugScreen from '../../screens/BugScreen';
+import VoteScreen from '../../screens/VoteScreen';
 
 /**
  * Creation d'un Stack Navigator à partir d'un Screen https://reactnavigation.org/docs/en/stack-navigator.html
@@ -12,7 +13,8 @@ import BugScreen from '../../screens/BugScreen';
 const HomeStack = createStackNavigator({
   	Home: HomeScreen,
     Faq: FaqScreen,
-    Bug: BugScreen
+    Bug: BugScreen,
+    Vote: VoteScreen
 });
 /**
  * Nom du label affiché dans le menu
