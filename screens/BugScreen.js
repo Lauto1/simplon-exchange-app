b443549943c9e2ed3eb0a397baccd65b7674ec33
tonoudo laurent <tonoudolaurent@gmail.com>
2019-04-26 15:12:11 +0000

    fusion inté et fonctionnel

--- screens/BugScreen.js
+++ screens/BugScreen.js
@@ -4,27 +4,28 @@ import BugReportComponent from "../components/BugReportComponent";
 import HeaderComponent from "../components/layouts/HeaderComponent";
 
 class BugScreen extends React.Component {
-  static navigationOptions = ({ navigation }) => {
-    return {
-      header: props => <HeaderComponent {...props} />
-    };
-  };
-  render() {
-    return (
-      <View style={styles.container}>
-        <BugReportComponent />
-      </View>
-    );
-  }
+
+    static navigationOptions = ({ navigation }) => {
+        return {
+            header: null,
+        };
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <BugReportComponent/>
+            </View>
+        );
+    }
 }
 
 export default BugScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center"
+      }
 });
