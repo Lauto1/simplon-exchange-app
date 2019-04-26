e7290a8d7bd4ea85c0403e575945aec8692a3052
LucaCarubia <luca.carubia42@gmail.com>
2019-04-26 10:33:30 +0200

    feature: ajout des checkbox, du bouton de recherche du filter compposant

--- components/Statistique.js
+++ components/Statistique.js
@@ -2,16 +2,19 @@ import React, {Component} from 'react';
 import { ScrollView, RefreshControl } from "react-native";
 import {View, StyleSheet, Platform, Text} from 'react-native';
 import Graph from '../containers/Graph'
+import Filter from '../containers/Filter'
+import Title from './titleDescription/Title.component';
+import Description from './titleDescription/Description.component';
 
 const Statistique = props => {
-    return (
-        <ScrollView>
-            <Text>Component de Sodalin</Text> 
-            <Text>Component de Tarik et Luca</Text>
-             
-            <Graph/>                   
-        </ScrollView>
-    )
+  return (
+    <ScrollView>
+      <Title/>
+      <Description />
+      <Filter/>
+      <Graph/>
+    </ScrollView>
+  )
 }
 
 export default Statistique;
--- components/filters/Filter.js
+++ components/filters/Filter.js
@@ -1,32 +1,74 @@
 import React, {Component} from 'react';
-import { ScrollView, RefreshControl } from "react-native";
-import {View, StyleSheet, Platform, Text} from 'react-native';
-import { StackedBarChart } from 'react-native-svg-charts'
-import { CheckBox } from 'react-native'
+import {View, Button, StyleSheet, Platform, Text} from 'react-native';
+import {CheckBox} from 'react-native-elements';
+
 class filter extends Component {
-  constructor(props){
+
+  constructor(props) {
     super(props);
     this.state = {
       refreshing: false,
-      NbPosts:0,
-      NbUtilisateurs:0,
-      NbReponses:0,
-      checked: false,
+      NbPosts: 0,
+      NbUsers: 0,
+      NbResponses: 0,
+      checkedNbPosts: false,
+      checkedNbUsers: false,
+      checkedNbResponses: false,
     }
   }
 
-  check(){
-    console.log('ok')
-  }
+
+  // récupere les données demander a l'api en fonction de son type (nombre de user, reponse, post)
+  fetchDataChecked = () => {
+
+    let checkedData = [];
+
+    if (this.checkedNbPosts === true) {
+      checkedData.push(this.checkedNbPosts)
+    }
+
+    if (this.checkedNbUsers === true) {
+      checkedData.push(this.checkedNbUsers)
+    }
+
+    if (this.checkedNbResponses === true) {
+      checkedData.push(this.checkedNbResponses)
+    }
+
+    this.props.actions.fetchDataApi(checkedData)
+  };
 
   render() {
 
     return (
 
       <View>
+
+        {/*
+        // Les checkboxs qui permet d'afficher les statistiques en fonction du type de donnée demander
+        */}
         <CheckBox
-          title='Click Here'
-          onPress={this.check()}
+          title='Trier par post'
+          checked={this.state.checkedNbPosts}
+          onPress={() => this.setState({checkedNbPosts: !this.state.checkedNbPosts})}
+        />
+
+        <CheckBox
+          title='Trier par utilisateur'
+          checked={this.state.checkedNbUsers}
+          onPress={() => this.setState({checkedNbUsers: !this.state.checkedNbUsers})}
+        />
+
+        <CheckBox
+          title='Trier par reponse'
+          checked={this.state.checkedNbResponses}
+          onPress={() => this.setState({checkedNbResponses: !this.state.checkedNbResponses})}
+        />
+
+        <Button
+          onPress={this.fetchDataChecked}
+          title="Rechercher"
+          color="#841584"
         />
       </View>
     )
