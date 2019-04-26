a9487f33ef39c324c3c069a74ff629265230e6c4
tonoudo laurent <tonoudolaurent@gmail.com>
2019-04-26 15:12:11 +0000

    fusion inté et fonctionnel

--- screens/BugScreen.js
+++ screens/BugScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import BugReportComponent from '../components/BugReportComponent'
 
 class BugScreen extends React.Component {
@@ -11,7 +11,7 @@ class BugScreen extends React.Component {
     }
     render() {
         return (
-            <View>
+            <View style={styles.container}>
                 <BugReportComponent/>
             </View>
         );
@@ -19,3 +19,12 @@ class BugScreen extends React.Component {
 }
 
 export default BugScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center"
+      }
+});
