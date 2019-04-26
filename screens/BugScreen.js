f4e777caf967441f11aec98dd0f9974c5575513f
Laurie.M <laurie.martineau.ext@homeserve.fr>
2019-04-26 12:16:04 +0200

    changes

--- screens/BugScreen.js
+++ screens/BugScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import Formulaire from '../components/Formulaire'
+import BugReportComponent from '../components/Formulaire'
 
 class BugScreen extends React.Component {
 
@@ -12,7 +12,7 @@ class BugScreen extends React.Component {
     render() {
         return (
             <View>
-                <Formulaire/>
+                <BugReportComponent/>
             </View>
         );
     }
