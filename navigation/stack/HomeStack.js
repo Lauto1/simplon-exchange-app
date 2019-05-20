6d36a864b3258e869c80ab9d61d587c1b833c366
Laurie <laurie.martineau8@gmail.com>
2019-05-20 14:54:26 +0200

    delete VoteScreen

--- navigation/stack/HomeStack.js
+++ navigation/stack/HomeStack.js
@@ -5,7 +5,6 @@ import HomeScreen from '../../screens/HomeScreen';
 import QuestionScreen from '../../screens/QuestionScreen';
 import FaqScreen from '../../screens/FaqScreen';
 import BugScreen from '../../screens/BugScreen';
-import VoteScreen from '../../screens/VoteScreen';
 
 /**
  * Creation d'un Stack Navigator à partir d'un Screen https://reactnavigation.org/docs/en/stack-navigator.html
@@ -14,8 +13,7 @@ const HomeStack = createStackNavigator({
   Home: HomeScreen,
   Question: QuestionScreen,
   Faq: FaqScreen,
-  Bug: BugScreen,
-  Vote: VoteScreen
+  Bug: BugScreen
 });
 /**
  * Nom du label affiché dans le menu
