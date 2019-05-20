a5b73e67f471b5bf4606498066d3c231bfa4717c
Franck <patouillardfranck3@gmail.com>
2019-05-20 13:36:25 +0200

    feat(GESTION DES VOTES): visibilité
    Création du component

---
+++ components/VoteSystemComponent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+export default class VoteSystemComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state({
+            isConnected:false
+        });
+    }
+    render() {
+        return (
+            <Provider store={store}>
+                {this.state.isConnected}
+            </Provider>
+        );
+    }
+}
