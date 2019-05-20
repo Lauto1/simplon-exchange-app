888b7d917fba9c5500e8e0d907db700c3457fc44
Diallo Alpha <jackobdiallo@gmail.com>
2019-05-20 15:06:05 +0200

    add link mailto && custom items footer

--- components/layouts/FooterComponent.js
+++ components/layouts/FooterComponent.js
@@ -5,83 +5,114 @@ import {
   Linking,
   View,
   Text,
+  Button,
   StyleSheet
 } from "react-native";
+// import DropdownMenu from 'react-native-dropdown-menu';
 
+class Anchor extends React.Component {
+  /**Faire un link pour le mailto */
+  _handlePress = () => {
+    console.log("Link clicked for " + this.props.href);
+    Linking.openURL(this.props.href);
+    this.props.onPress && this.props.onPress();
+  };
+
+  render() {
+    return (
+      <TouchableOpacity
+        style={{ borderRadius: 5 }}
+        onPress={() => console.log("By Simplon-Vénissieux")}
+      >
+         <Text style={styles.titleMailto} title={this.props.title} onPress={this._handlePress}>By Simplon-Vénissieux</Text>
+      </TouchableOpacity>
+    );
+  }
+};
 
 class FooterComponent extends Component {
+
+  /**Faire un dropdown menu pour les elements du footer */
+    constructor(props) {
+      super(props);
+      this.state = {
+        text: ''
+      };
+    }
+
   /***Composant FOOTER connecter au screen (HomeScreen) & re-utilisable comme le HEADER dans les autres composants*/
   render() {
     return (
       <ScrollView>
         <View style={styles.wrapper}>
-          <View>
-            <Text style={styles.titleText}>Exchange ?</Text>
-            <Text style={styles.textStyle}>
-              Un lieu d'échange convivial où toutes questions trouvera une
-              réponse. Apprentissage de la pédagogie de l'entraide. Savoir
-              questionner pour obtenir des réponses pertinentes.
-            </Text>
-          </View>
 
-          <View>
-            <Text style={styles.titleText}>Liens utiles</Text>
-            <View
-              style={{
-                flexDirection: "column"
-              }}
+              <View>
+                <Text style={styles.titleText}>Exchange ?</Text>
+                <Text style={styles.textStyle}>
+                  Un lieu d'échange convivial où toutes questions trouvera une
+                  réponse. Apprentissage de la pédagogie de l'entraide. Savoir
+                  questionner pour obtenir des réponses pertinentes.
+                </Text>
+              </View>
+
+              <View>
+                <Text style={styles.titleText}>Liens utiles</Text>
+                <View
+                  style={{
+                    flexDirection: "column"
+                  }}
+                >
+                  <TouchableOpacity
+                    style={{ borderRadius: 5 }}
+                    onPress={() => console.log("Accueil route")}
+                  >
+                    <Text style={styles.textStyle}>Accueil</Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity
+                    style={{ borderRadius: 5 }}
+                    onPress={() => console.log("poser une question route")}
+                  >
+                    <Text style={styles.textStyle}>Poser une question</Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity
+                    style={{ borderRadius: 5 }}
+                    onPress={() => console.log("aider la communauté route")}
+                  >
+                    <Text style={styles.textStyle}>Aider la communauté</Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity
+                    style={{ borderRadius: 5 }}
+                    onPress={() => console.log("foire au questions route")}
+                  >
+                    <Text style={styles.textStyle}>Foire Aux Questions</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+
+              <View>
+                <Text style={styles.titleText}>Intéret ?</Text>
+                <Text style={styles.textStyle}>
+                  Créer en tant que projet back office pour la fabrique de Roanne,
+                  et outil à vocation pédagogique a pour objectif, de servir de lieu
+                  d'échange entre tous simploniens de toutes promotions.
+                </Text>
+              </View>
+          </View>     
+
+          <View style={styles.Copyright}>
+            <Text style={{ color: "#fff", paddingBottom: 8 }}>
+              Copyright 2019 Une Question ? |
+            </Text>
+            <TouchableOpacity
+              style={{ width: "90%", borderRadius: 5 }}
+              onPress={() => console.log("mail to")}
             >
-              <TouchableOpacity
-                style={{ borderRadius: 5 }}
-                onPress={() => console.log("Accueil route")}
-              >
-                <Text style={styles.textStyle}>Accueil</Text>
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                style={{ borderRadius: 5 }}
-                onPress={() => console.log("poser une question route")}
-              >
-                <Text style={styles.textStyle}>Poser une question</Text>
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                style={{ borderRadius: 5 }}
-                onPress={() => console.log("aider la communauté route")}
-              >
-                <Text style={styles.textStyle}>Aider la communauté</Text>
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                style={{ borderRadius: 5 }}
-                onPress={() => console.log("foire au questions route")}
-              >
-                <Text style={styles.textStyle}>Foire Aux Questions</Text>
-              </TouchableOpacity>
-            </View>
+              <Anchor style={styles.CopyrightItems} href="mailto://contact@idmkr.io" title="By Simplon-Vénissieux" />
+            </TouchableOpacity>
           </View>
-
-          <View>
-            <Text style={styles.titleText}>Intéret ?</Text>
-            <Text style={styles.textStyle}>
-              Créer en tant que projet back office pour la fabrique de Roanne,
-              et outil à vocation pédagogique a pour objectif, de servir de lieu
-              d'échange entre tous simploniens de toutes promotions.
-            </Text>
-          </View>
-        </View>
-
-        <View style={styles.Copyright}>
-          <Text style={{ color: "#fff", paddingBottom: 8 }}>
-            Copyright 2019 Une Question ? |
-          </Text>
-          <TouchableOpacity
-            style={{ width: "90%", borderRadius: 5 }}
-            onPress={() => console.log("mail to")}
-          >
-            <Text style={styles.CopyrightItems}>By Simplon-Vénissieux</Text>
-          </TouchableOpacity>
-        </View>
       </ScrollView>
     );
   }
@@ -133,6 +164,10 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 14,
     fontFamily: "firacode"
+  },
+  titleMailto: {
+    color: "#0056b3",
+    textAlign: "center",
   }
 });
 
