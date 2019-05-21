9fa9144fef45ab9192652f55935a63e73ba185a9
sodalin.seng@lyaprotect.com <sodalin.seng@gmail.com>
2019-05-21 12:54:54 +0200

    Résoudre des conflits 2

--- components/Graph.js
+++ components/Graph.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
-import { StackedBarChart } from "react-native-svg-charts";
-import { lightGreyColor } from "../helpers/styleGuidelines";
+import React, { Component } from 'react';
+import { ScrollView, RefreshControl } from 'react-native';
+import { View, StyleSheet, Platform, Text } from 'react-native';
+import { StackedBarChart } from 'react-native-svg-charts';
 class graph extends Component {
   render() {
     /*
@@ -18,13 +18,12 @@ class graph extends Component {
       },
       {
         month: new Date(2015, 2, 1),
-        element: this.props.NbResponses
+        element: this.props.NbReponses
       }
     ];
 
-<<<<<<< HEAD
-    const colors = ["#18275f"];
-    const keys = ["element"];
+    const colors = ['#d6363e'];
+    const keys = ['element'];
     return (
       /*
        **event swipe pour raffraichir les data.
@@ -35,7 +34,7 @@ class graph extends Component {
                 configuration chart graph.
                 */}
           <StackedBarChart
-            style={{ height: 200, backgroundColor: lightGreyColor }}
+            style={{ height: 200, backgroundColor: '#2F3239' }}
             keys={keys}
             colors={colors}
             data={data}
@@ -46,7 +45,7 @@ class graph extends Component {
           <Text style={styles.text}>
             Utilisateurs: {this.props.NbUtilisateurs}
           </Text>
-          <Text style={styles.text}>Reponses: {this.props.NbResponses}</Text>
+          <Text style={styles.text}>Reponses: {this.props.NbReponses}</Text>
         </View>
       </ScrollView>
     );
@@ -57,69 +56,9 @@ const styles = StyleSheet.create({
     margin: 20
   },
   text: {
-    color: "#00008B",
+    color: '#2F3239',
     marginTop: 20
   }
 });
 
 export default graph;
-=======
-        /*
-        **data du chart graph qui vont etre affichés.
-        */
-        const data = [
-            {
-                month: new Date(2015, 0, 1),
-                element: this.props.NbPosts,
-                
-            },
-            {
-                month: new Date(2015, 1, 1),
-                element: this.props.NbUtilisateurs,
-                
-            },
-            {
-                month: new Date(2015, 2, 1),
-                element: this.props.NbResponses,                
-            },
-        ]
- 
-        const colors = ['#d6363e'];
-        const keys   = [ 'element',]
-        return (
-            /*
-            **event swipe pour raffraichir les data.
-            */
-            <ScrollView 
-            style={{margin: 10}}
-            >
-            <View>
-                {/* 
-                configuration chart graph.
-                */}
-                <StackedBarChart
-                style={ {  height: 200, backgroundColor: '#2F3239', } }
-                keys={ keys }
-                colors={ colors }
-                data={ data }
-                showGrid={ false }
-                contentInset={ { top: 30} }
-            />
-            <Text style={styles.text}>Posts: {this.props.NbPosts}</Text>
-            <Text style={styles.text}>Utilisateurs: {this.props.NbUtilisateurs}</Text>
-            <Text style={styles.text}>Reponses: {this.props.NbResponses}</Text>
-
-            </View> 
-            </ScrollView>
-        )
-    }
-    }
-    const styles = StyleSheet.create({
-          text: {
-            color: '#2F3239',
-            margin: 20
-          }
-      });
- 
-export default graph;
->>>>>>> design integration graphique
