e5112b0f5aff9474ef967a62a8f57330a8fa89dc
Laurie <laurie.martineau8@gmail.com>
2019-05-22 09:52:34 +0200

    changes

--- helpers/faq.js
+++ helpers/faq.js
@@ -1,82 +1,82 @@
-export const faq = [
+export const faq =[
   {
     "id": 1,
-    "question": "Comment expédier mon colis ?",
-    "answer": "1. Renseignez vos informations d'expédition. A noter que pour les expéditions multiples, vous ne pouvez pas envoyer plus de 5 colis vers un même destinataire.\n2. Sélectionnez l'offre correspondant à vos besoins.\n3. Sélectionnez vos options et le mode de prise en charge de votre colis\n4. Complétez les coordonnées expéditeur, destinataire \n5. Validez et réglez par carte bancaire, Paypal ou prélèvement. "
+    "question": "Lorem ipsum dolor sit amet",
+    "answer": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
-    "id":  2,
-    "question": "Qu'est-ce que les droits de douanes et les taxes à l'importation et comment les calculer ?",
-    "answer": "Qu'est-ce que les droits de douanes et les taxes à l'importation et comment les calculer ?"
+    "id": 2,
+    "question": "Lorem ipsum dolor sit amet",
+    "answer": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
     "id": 3,
-    "question": "A qui sont facturés les droits de douane et les taxes d'importation ?",
-    "answer": "Les droits et taxes sont réclamés au destinataire de l'envoi au moment de la livraison."
+    "question": "Lorem ipsum dolor sit amet",
+    "answer": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
     "id": 4,
-    "question": "Dans quel cas utiliser une facture pro forma ou une facture commerciale ?",
-    "answer": "Dans quel cas utiliser une facture pro forma ou une facture commerciale ?"
+    "question": "Lorem ipsum dolor sit amet",
+    "answer": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
     "id": 5,
-    "question": "Qu'est-ce qu'une Prior Notice ?",
-    "answer": "Pour exporter et faciliter le contrôle des produits alimentaires vers les Etats-Unis, vous devez être enregistré auprès de la Food and Drug Administration (FDA) et émettre une Prior notice (PN)"
+    "question": "Lorem ipsum dolor sit amet",
+    "answer": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
     "id": 6,
-    "question": "Le SMS/email que j'ai reçu a-t-il bien été envoyé par Chronopost ?",
-    "answer": "Si le message provient de Chronopost, l'émetteur du message sera :"
+    "question": "Lorem ipsum dolor sit amet?",
+    "answer": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
     "id": 7,
-    "question": "Comment obtenir la preuve de livraison de mon colis ?",
-    "answer": "Si vous êtes le destinataire :Nous vous invitons à vous rapprocher de votre expéditeur afin d'obtenir la preuve de livraison de votre colis."
+    "question": "Lorem ipsum dolor sit amet",
+    "answer": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
     "id": 8,
-    "question": "Où trouver mon numéro de suivi de colis ?",
-    "answer": "Le numéro de suivi est composé de 13 caractères : 2 lettres + 9 chiffres + 2 lettres (ex: XX123456789FR) ou 15 caractères pour les colis en provenance de l'étranger.(Il doit être saisi sans espace sur notre suivi en ligne) Si vous êtes le destinataire et que vous souhaitez suivre votre colis : Vous l'avez reçu par email ou par SMS (si ce n'est pas le cas, regardez dans vos spams ou faites-en la demande auprès de votre expéditeur).Si vous êtes l'expéditeur et que vous ne trouvez pas le numéro de colis :Le numéro de colis est présent sur le \"Feuillet Expéditeur\" que vous avez conservé comme preuve de dépôt. "
+    "question": "Lorem ipsum dolor sit amet",
+    "answer": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
     "id": 9,
-    "question": "Comment expédier mon colis ?",
-    "answer": "1. Renseignez vos informations d'expédition. A noter que pour les expéditions multiples, vous ne pouvez pas envoyer plus de 5 colis vers un même destinataire.\n2. Sélectionnez l'offre correspondant à vos besoins.\n3. Sélectionnez vos options et le mode de prise en charge de votre colis\n4. Complétez les coordonnées expéditeur, destinataire \n5. Validez et réglez par carte bancaire, Paypal ou prélèvement. "
+    "question": "Lorem ipsum dolor sit amet",
+    "answer": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
-    "id":  10,
-    "question": "Qu'est-ce que les droits de douanes et les taxes à l'importation et comment les calculer ?",
-    "answer": "Qu'est-ce que les droits de douanes et les taxes à l'importation et comment les calculer ?"
+    "id": 10,
+    "question": "Lorem ipsum dolor sit amet",
+    "answer": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
     "id": 11,
-    "question": "A qui sont facturés les droits de douane et les taxes d'importation ?",
-    "answer": "Les droits et taxes sont réclamés au destinataire de l'envoi au moment de la livraison."
+    "question": "Lorem ipsum dolor sit amet",
+    "answer": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
     "id": 12,
-    "question": "Dans quel cas utiliser une facture pro forma ou une facture commerciale ?",
-    "answer": "Dans quel cas utiliser une facture pro forma ou une facture commerciale ?"
+    "question": "Lorem ipsum dolor sit amet",
+    "answer": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
     "id": 13,
-    "question": "Qu'est-ce qu'une Prior Notice ?",
-    "answer": "Pour exporter et faciliter le contrôle des produits alimentaires vers les Etats-Unis, vous devez être enregistré auprès de la Food and Drug Administration (FDA) et émettre une Prior notice (PN)"
+    "question": "Lorem ipsum dolor sit amet",
+    "answer": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
     "id": 14,
-    "question": "Le SMS/email que j'ai reçu a-t-il bien été envoyé par Chronopost ?",
-    "answer": "Si le message provient de Chronopost, l'émetteur du message sera :"
+    "question": "Lorem ipsum dolor sit amet",
+    "answer": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
     "id": 15,
-    "question": "Comment obtenir la preuve de livraison de mon colis ?",
-    "answer": "Si vous êtes le destinataire :Nous vous invitons à vous rapprocher de votre expéditeur afin d'obtenir la preuve de livraison de votre colis."
+    "question": "Lorem ipsum dolor sit amet",
+    "answer": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
     "id": 16,
-    "question": "Où trouver mon numéro de suivi de colis ?",
-    "answer": "Le numéro de suivi est composé de 13 caractères : 2 lettres + 9 chiffres + 2 lettres (ex: XX123456789FR) ou 15 caractères pour les colis en provenance de l'étranger.(Il doit être saisi sans espace sur notre suivi en ligne) Si vous êtes le destinataire et que vous souhaitez suivre votre colis : Vous l'avez reçu par email ou par SMS (si ce n'est pas le cas, regardez dans vos spams ou faites-en la demande auprès de votre expéditeur).Si vous êtes l'expéditeur et que vous ne trouvez pas le numéro de colis :Le numéro de colis est présent sur le \"Feuillet Expéditeur\" que vous avez conservé comme preuve de dépôt. "
+    "question": "Lorem ipsum dolor sit amet",
+    "answer": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   }
-  ]
+]
