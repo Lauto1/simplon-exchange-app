beda93003f9547515f757f940c994c4727a9ed69
tarik <tbouchenafa@simplon.co>
2019-05-23 11:42:05 +0200

    modif datePicker

--- actions/popUp.js
+++ actions/popUp.js
@@ -1,24 +1,33 @@
-import types from "../constants/actionTypes";
+import * as types from "../constants/actionTypes";
 
-export const fetchDataApi = checkedData => {
-  return async dispatch => {
-    // dispatch(toggleLoader(true));
-    // const response = await fetch('https://api/answers');
-    // const quote = await response.json();
+export const fetchDataApi = (checkedData) => {
+    return async dispatch => {
+        // dispatch(toggleLoader(true));
+        // const response = await fetch('https://api/answers');
+        // const quote = await response.json();
 
-    let data = { NbPosts: 150, NbResponses: 600, NbUsers: 224 };
-
-    dispatch(addNbPosts(data.NbPosts));
-    dispatch(addNbResponses(data.NbResponses));
-    dispatch(addNbUsers(data.NbUsers));
-
-    // dispatch(toggleLoader(false));
+    let data = {NbPosts:150, NbResponses:600, NbUsers:224}
+        
+     dispatch(addNbPosts(data.NbPosts));
+     dispatch(addNbResponses(data.NbResponses));
+     dispatch(addNbUsers(data.NbUsers));  
+     
+        // dispatch(toggleLoader(false));
+    }
   };
-};
 
-export const addNbPosts = NbPosts => ({ type: types.ADD_NB_POSTS, NbPosts });
-export const addNbResponses = NbResponses => ({
-  type: types.ADD_NB_RESPONSES,
-  NbResponses
-});
-export const addNbUsers = NbUsers => ({ type: types.ADD_NB_USERS, NbUsers });
+  export const setDatePickedStart = (DatePickedStart)=>(
+    console.log('date de debut'), 
+    DatePickedStart = JSON.stringify(DatePickedStart),  
+    {type: types.ADD_START_DATE_FILTER, DatePickedStart});
+
+  export const setDatePickedEnd = (DatePickedEnd)=>(
+        console.log('date de finzzzz:'),
+        DatePickedEnd =JSON.stringify(DatePickedEnd), 
+        console.log(DatePickedEnd),
+        {type: types.ADD_END_DATE_FILTER, DatePickedEnd});  
+
+  export const addNbPosts = (NbPosts) => ({type: types.ADD_NB_POSTS, NbPosts});
+  export const addNbResponses = (NbResponses) => ({type: types.ADD_NB_RESPONSES, NbResponses});
+  export const addNbUsers = (NbUsers) => ({type: types.ADD_NB_USERS, NbUsers});
+  
--- reducers/storeStatistiques.js
+++ reducers/storeStatistiques.js
@@ -5,31 +5,50 @@ import types from "../constants/actionTypes";
  */
 
 let initialState = {
-  NbPosts: 1000,
-  NbUtilisateurs: 2000,
-  NbResponses: 1500
+    NbPosts: 1000,
+    NbUtilisateurs:2000,
+    NbResponses:1500,
+    StartDateFilter:'',
+    EndDateFilter: ''
+
 };
 
 export const storeStatistiques = (state = initialState, action) => {
-  switch (action.type) {
-    case types.ADD_NB_POSTS:
-      return {
-        ...state,
-        NbPosts: action.NbPosts
-      };
-
-    case types.ADD_NB_RESPONSES:
-      return {
-        ...state,
-        NbResponses: action.NbResponses
-      };
-
-    case types.ADD_NB_USERS:
-      return {
-        ...state,
-        NbUsers: action.NbUsers
-      };
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case types.ADD_NB_POSTS:
+            return {
+                ...state,
+                NbPosts: action.NbPosts
+            };       
+    
+        case types.ADD_NB_RESPONSES:
+            return {
+                ...state,
+                NbResponses: action.NbResponses
+            };
+        
+        
+        case types.ADD_NB_USERS:
+            console.log(action.NbUsers)
+            return {
+                ...state,
+                NbUsers: action.NbUsers
+            };
+        case types.ADD_END_DATE_FILTER:
+            console.log('tarik test')
+            console.log(action.DatePickedEnd)
+            return {                
+                ...state,
+                EndDateFilter: 23
+            };
+        case types.ADD_START_DATE_FILTER:
+        console.log(action.DatePickedStart)
+            
+            return {                
+                ...state,
+                StartDateFilter: 24
+            };
+        default:
+            return state;
+    }
 };
