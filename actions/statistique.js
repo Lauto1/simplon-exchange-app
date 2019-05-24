af3bb351d403e4fdc8dfb431df47ecbc0a142b23
Franck <patouillardfranck3@gmail.com>
2019-05-24 11:53:14 +0200

    adding url api

--- actions/statistique.js
+++ actions/statistique.js
@@ -3,3 +3,8 @@ import types from "../constants/actionTypes";
 export const getNbPost = (NbPosts) => {
 //   return { type: types.GET_POSTS, content: NbPosts };
 };
+
+export const getProfilUser = () => {
+    const base = 'http://dev.simplon-exchange.help/api';
+    const req  = base+'/users';
+}
