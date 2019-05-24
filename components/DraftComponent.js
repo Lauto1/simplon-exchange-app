f1944e65968a1e9588dd5240e1e9420cc3ae8899
Diallo Alpha <jackobdiallo@gmail.com>
2019-05-24 15:25:34 +0200

    add KeyboardAvoidingView

--- components/DraftComponent.js
+++ components/DraftComponent.js
@@ -1,17 +1,8 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Alert, Button, ScrollView, TextInput } from "react-native";
-import { Card, Icon } from "react-native-elements";
-import {
-    primaryColor,
-    lightGreyColor,
-    boldFontFamily,
-    titleFontSize,
-    whiteColor,
-    blackColor,
-    secondaryTextColor,
-    regularFontFamily,
-    paragraphFontSize
-  } from "../helpers/styleGuidelines";
+import { Button, KeyboardAvoidingView, StyleSheet, Text, TextInput, View } from "react-native";
+import { primaryColor, regularFontFamily, secondaryTextColor } from "../helpers/styleGuidelines";
+
+//   import {KeyboardAvoidingView} from 'react-native';
 //import getRNDraftJSBlocks from 'react-native-draftjs-render';
 
 /**TODO Changer le text input par un editeur editeur wysiwyg */
@@ -26,16 +17,19 @@ class DraftComponent extends Component {
     }
     render() {
         return (
-            <ScrollView style={{ flex: 1 }}><TextInput multiline={true} style={styles.textInput}
-                numberOfLines={8}
-                onChangeText={(text) => this.getText(text)}
-                value={this.state.text} placeholder="Entrez votre réponse" />
-                {this.props.showSubInput ? <Text style={styles.subInput}>
-                  Ecrire la description de votre problème en entrant tous les
-                  détails possibles qui en permettront la résolution.
-                </Text> : null}
-                <Button color={primaryColor} title="Répondre" onPress={() => { this.onTextChange() }}></Button>
-                </ScrollView>
+            <KeyboardAvoidingView behavior="position" enabled>
+                <View>
+                    <TextInput multiline={true} style={styles.textInput}
+                        numberOfLines={8}
+                        onChangeText={(text) => this.getText(text)}
+                        value={this.state.text} placeholder="Entrez votre réponse" />
+                        {this.props.showSubInput ? <Text style={styles.subInput}>
+                        Ecrire la description de votre problème en entrant tous les
+                        détails possibles qui en permettront la résolution.
+                    </Text> : null}
+                    <Button color={primaryColor} title="Répondre" onPress={() => { this.onTextChange() }}></Button>
+                </View>               
+            </KeyboardAvoidingView>
         );
     }
     onTextChange() {
