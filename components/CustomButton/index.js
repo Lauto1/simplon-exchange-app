36a923e2777cffe52f9cffbac9551743df9c3e50
Clément Causer <clement.causer@gmail.com>
2019-05-24 11:47:17 +0200

    Added documentation

--- components/CustomButton/index.js
+++ components/CustomButton/index.js
@@ -2,12 +2,17 @@ import React from "react";
 import { StyleSheet, Text, TouchableOpacity } from "react-native";
 import { primaryColor, subtitleFontSize } from "../../helpers/styleGuidelines";
 
+/**
+ * This component takes 3 props :
+ * @param {function} onPress This prop calls the parents function (where you handle the action)
+ * @param {string} title This is the text content inside the button
+ * @param {string} backgroundColor This is the background color for the button
+ */
 const CustomButton = ({ onPress, title, backgroundColor }) => {
   return (
     <TouchableOpacity
       onPress={onPress}
-      style={[styles.wrapper, { backgroundColor }]}
-    >
+      style={[styles.wrapper, { backgroundColor }]}>
       <Text style={styles.text}>{title}</Text>
     </TouchableOpacity>
   );
